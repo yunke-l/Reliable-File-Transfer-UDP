fdef03144332188befaf19ba7ca797ef79d76292
peihao <pei.h@northeastern.edu>
2024-11-09 14:51:44 -0800

    removed the test files

--- UDP_client.py
+++ UDP_client.py
@@ -1,80 +1,149 @@
-import struct
 import socket
+import struct
 import sys
+from struct import pack, unpack
 
 
-def create_socket():
+CLIENT_IP = '127.0.0.1'
+SERVER_IP = '127.0.0.1'
+
+CLIENT_PORT = 3434
+SERVER_PORT = 50100
+
+
+def receive_udp(ip, port, single):
+    payloads = []
+
     try:
-        newsocket = socket.socket(socket.AF_INET, socket.SOCK_RAW,
-                                  socket.IPPROTO_UDP)
-    except socket.error as e:
-        print(f'Socket creation failed: {e}')
+        server_socket = socket.socket(
+            socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+    except socket.error as msg:
+        print('Socket could not be created. Error Code : ' +
+              str(msg[0]) + ' Message ' + msg[1])
         sys.exit()
-    return newsocket
+
+    server_ip = ip
+    server_port = port
+
+    try:
+        server_socket.bind((server_ip, server_port))
+    except socket.error as msg:
+        print('Can not bind socket. Error Code : ' +
+              str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
+
+    print(f"Server listening on {server_ip}:{server_port}")
+
+    try:
+        while True:
+            data, addr = server_socket.recvfrom(65535)
+            print(f"Received message from {addr}")
+            print('data as hex:', data.hex())
+            print('data length:', len(data))
+
+            if (len(data) > 28):
+                ip_header = data[0:20]
+                print('ip_header:', ip_header.hex())
+
+            # need to skip the first 20 header.
+            # Looks like kernal add one additional layer of ip header to package.
+            # Unpack IP header
+            # ip_header = data[20:40]
+            # iph = unpack('!BBHHHBBH4s4s', ip_header)
+            # ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr = iph
+            # print(ip_proto)
+
+            # Unpack UDP header
+            udp_header = data[20:28]
+            udp_unpack = unpack('!HHHH', udp_header)
+
+            source_port, dest_port, length, checksum = udp_unpack
+            # print(f"UDP Source Port: {source_port}, Dest Port: {dest_port}, Length: {length}")
+
+            payload = data[28:]
+
+            if single:
+                string_payload = payload.decode('utf-8')
+                print(f"Payload:\n{string_payload}")
+                return payload
+            else:
+                num_chunks_info = data[48:52]
+                num_chunks = struct.unpack('!I', num_chunks_info)[0]
+
+                index_info = data[52:56]
+                index = struct.unpack('!I', index_info)[0]
+
+                payloads.append(payload)
+                print(f"received {index}/{num_chunks-1}")
+                if len(payloads) >= num_chunks:
+                    print(f"Received last chunk woo ho!")
+                    return payloads
+
+    except KeyboardInterrupt:
+        print('Server shutting down.')
+    finally:
+        server_socket.close()
 
 
-def calculate_checksum(segment):
-    if len(segment) % 2 != 0:
-        segment += b'\0'  # Padding if segment length is odd
+def send_udp(payload: bytes, dst_ip, src_port, dst_port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+    except socket.error as msg:
+        print('Socket could not be created. Error Code : ' +
+              str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
 
+    # # construct UDP header
+    # ip_ihl = 5
+    # ip_ver = 4
+    # ip_tos = 0
+    # ip_tot_len = 0  # kernel will fill the correct total length
+    # ip_id = 12345
+    # ip_frag_off = 0x4000
+    # ip_ttl = 40
+    # ip_proto = socket.IPPROTO_UDP
+    # ip_check = 0
+    # ip_saddr = socket.inet_aton(src_ip)
+    # ip_daddr = socket.inet_aton(dst_ip)
+    # ip_ihl_ver = (ip_ver << 4) + ip_ihl
+
+    # ip_header = pack('!BBHHHBBH4s4s', ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check,
+    #                  ip_saddr, ip_daddr)
+
+    # construct UDP header
+    udp_src_port = src_port
+    udp_dst_port = dst_port
+    udp_length = 8 + len(payload)
     checksum = 0
-    for i in range(0, len(segment), 2):
-        word = (segment[i] << 8) + segment[i + 1]
-        checksum += word
+    udp_header = pack('!HHHH', udp_src_port,
+                      udp_dst_port, udp_length, checksum)
 
-    checksum = (checksum >> 16) + (checksum & 0xFFFF)
-    checksum += (checksum >> 16)
+    packet = udp_header + payload
 
-    return ~checksum & 0xFFFF
-
-def create_UDP_segment():
-    file_name = input('Enter the file name: ')
-    # udp_src_port = 12345
-    udp_dest_port = 7001
-    udp_length = 8 + len(file_name.encode())
-    udp_checksum = 0
-    udp_header = struct.pack('!HHH', udp_dest_port,
-                             udp_length, udp_checksum)
-    segment = udp_header + file_name.encode()
-    udp_checksum = calculate_checksum(segment)
-    udp_header = struct.pack('!HHH', udp_dest_port,
-                             udp_length, udp_checksum)
-    return udp_header + file_name.encode()
+    s.sendto(packet, (dst_ip, dst_port))
 
 
-def create_IP_header():
-    src_ip = '127.0.0.1'  # loopback address
-    dest_ip = '127.0.0.1'  # loopback address
-    ip_version = 4
-    ip_header_length = 5
-    ip_tos = 0
-    ip_total_length = 0
-    ip_id = 54321
-    ip_frag_offset = 0
-    ip_ttl = 255
-    ip_protocol = socket.IPPROTO_UDP
-    ip_checksum = 0
-    ip_src_addr = socket.inet_aton(src_ip)
-    ip_dest_addr = socket.inet_aton(dest_ip)
-    ip_ihl_ver = (ip_version << 4) + ip_header_length
-    ip_header = struct.pack('!BBHHHBBH4s4s', ip_ihl_ver, ip_tos,
-                            ip_total_length, ip_id, ip_frag_offset,
-                            ip_ttl, ip_protocol, ip_checksum,
-                            ip_src_addr, ip_dest_addr)
-    return ip_header
+def receive_file_chunks(ip, port):
+    return receive_udp(ip, port, True)
 
 
-def main():
-    udp_segment = create_UDP_segment()
-    ip_header = create_IP_header()
-    packet = ip_header + udp_segment
-
-    client_socket = create_socket()
-    client_socket.sendto(packet, ('127.0.0.1', 0))
-    # modifiedMessage, serverAddress = client_socket.recvfrom(65535)
-    # print('From the server: ', modifiedMessage.decode())
-    client_socket.close()
+def save_bytes_to_file(file_bytes, file_name):
+    with open(file_name, 'wb') as file:
+        file.write(file_bytes)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    file_name = input("File name: ")
+    file_name_bytes = file_name.encode('utf-8')
+    send_udp(file_name_bytes, SERVER_IP,
+             CLIENT_PORT, SERVER_PORT)
+
+    payloads = receive_file_chunks(CLIENT_IP, CLIENT_PORT)
+
+    received_file_bytes = b''.join(payload[8:] for payload in payloads)
+
+    new_file_name = "copy_" + file_name
+
+    save_bytes_to_file(received_file_bytes, new_file_name)
+
+    print(f"Saved the received file to {new_file_name}.")
