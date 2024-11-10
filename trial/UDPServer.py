5118181f8a43fee707085bedd4b34c74be03e45a
peihao <pei.h@northeastern.edu>
2024-11-09 16:56:10 -0800

    updated the client and server file

--- trial/UDPServer.py
+++
@@ -1,9 +0,0 @@
-from socket import *
-serverPort = 7001
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-print(f'The server is listening on {serverPort}')
-while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
