8e1641cd2eb2c72f064bc2328c20f63266d8ce73
peihao <pei.h@northeastern.edu>
2024-11-09 16:51:34 -0800

    stop tracking test file: trial folder.

--- trial/UDPClient.py
+++
@@ -1,9 +0,0 @@
-from socket import *
-serverName = gethostname() # the loopback address, refers to the IP address of the machine you are running the client on.
-serverPort = 7001
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input a string:')
-clientSocket.sendto(message.encode(),(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(f'From the server {serverName}: ', modifiedMessage.decode())
-clientSocket.close()
